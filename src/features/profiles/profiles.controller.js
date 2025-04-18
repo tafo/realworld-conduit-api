4212ce694802508d16744fb0764b1ea2506b9e5a
Tayfun Yirdem <tayfunyirdem@gmail.com>
2025-04-18 12:46:20 +0300

    unfollow user is implemented

--- src/features/profiles/profiles.controller.js
+++ src/features/profiles/profiles.controller.js
@@ -36,7 +36,32 @@ const followUser = async (req, res) => {
   return res.status(200).json({ profile: { ...getProfileResponse(targetUser, currentUser), following: true } });
 };
 
+const unfollowUser = async (req, res) => {
+  const currentUser = req.user;
+  const { username } = req.params;
+
+  const targetUser = await User.findOne({ username });
+  if (!targetUser) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  if (targetUser._id.toString() === currentUser.id) {
+    return res.status(422).json({ error: "You cannot unfollow yourself" });
+  }
+
+  await User.findByIdAndUpdate(currentUser.id, {
+    $pull: { following: targetUser._id },
+  });
+
+  await User.findByIdAndUpdate(targetUser._id, {
+    $pull: { followers: currentUser.id },
+  });
+
+  return res.status(200).json({ profile: { ...getProfileResponse(targetUser, currentUser), following: false } });
+};
+
 module.exports = {
   getProfile,
   followUser,
+  unfollowUser,
 };
--- src/features/profiles/profiles.router.js
+++ src/features/profiles/profiles.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const public = require('../../middlewares/public.middleware');
 const auth = require('../../middlewares/auth.middleware');
-const { getProfile, followUser } = require('./profiles.controller');
+const { getProfile, followUser, unfollowUser } = require('./profiles.controller');
 
 const profilesRouter = express.Router();
 
 profilesRouter.get('/:username', public, getProfile);
 profilesRouter.post('/:username/follow', auth, followUser);
+profilesRouter.delete('/:username/follow', auth, unfollowUser);
 
 module.exports = profilesRouter;
